5157ef2da92d5222adc6e3f500d7aeb4abfc4680
Givaldo-Andrade <81401949+Givaldo-Andrade@users.noreply.github.com>
2023-10-01 22:50:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,32 @@ var btn = window.document.getElementById('b_enviar')
 function enviar() {
     const inputNome = document.querySelector('.nome_form');
     var nome = inputNome.value
+    nome.trim()
     const inputEmail = document.querySelector('.email_form');
     var email = inputEmail.value
+    email.trim()
     const inputTelefone = document.querySelector('.telefone_form');
     var telefone = inputTelefone.value
+    telefone.trim()
     const inputProduto = document.querySelector('.produto_form');
     var produto = inputProduto.value
+    produto.trim()
     const selectMarca = document.querySelector('.select_marca');
     var marca = selectMarca.value
     const areaMensagem = document.querySelector('.mensagem_form');
     var mensagem = areaMensagem.value
-    
-    if (email.includes("@")) {
-        var emailValid = true;
+    mensagem.trim()
+
+    if (nome.length >= 6 && isNaN(nome)) {
+        nomeValid = true
+
+    } else {
+        alert("Nome inválido.")
+        inputNome.value = ''
+    }
+
+    if (email.includes("@") && email.includes(".") && email.length >=8 && email.includes(" ") == false) {
+        emailValid = true;
 
     } else {
         if (email != '') { 
@@ -23,16 +36,17 @@ function enviar() {
             inputEmail.value = ''
         }
     }
-    
-    if (isNaN(telefone)) {
-        alert("O número de telefone digitado não é um número.")
-        inputTelefone.value = ''
+
+    if (isNaN(telefone) == false && telefone.length >= 12 && telefone.includes(" ") == false) {
+        numeroValid = true
         
+                
     } else {
-        var numeroValid = true
+        alert("Telefone inválido.")
+        inputTelefone.value = ''
     }
     
-    if (nome != "" && emailValid && email != "" && numeroValid && telefone != "" && produto != "" && marca != "" && mensagem != "") {
+    if (nomeValid && emailValid && numeroValid && produto != "" && marca != "" && mensagem != "") {
         alert(`Ok ${nome}! Sua mensagem foi enviada com sucesso!\nConte conosco sempre que precisar :)`)
         inputNome.value = '';
         inputEmail.value = '';
@@ -43,4 +57,5 @@ function enviar() {
         window.location.href = "index.html";
 
     }
+
 }
